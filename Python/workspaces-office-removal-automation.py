fcdfec01d8b76aebe69ceb9a1cd507b1804f30f7
Justin Grego <97710986+JGrego-AWS@users.noreply.github.com>
2023-11-01 16:57:13 -0400

    Update workspaces-office-removal-automation.py

--- Python/workspaces-office-removal-automation.py
+++ Python/workspaces-office-removal-automation.py
@@ -15,13 +15,26 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-## Upgrade Boto3, v.1.28 required for WorkSpaces application management API, adds ~5 seconds to execution time
+## Upgrade Boto3, v.1.28 required for WorkSpaces application management API
+## Adds ~5 seconds to execution time
 ## AWS Lambda with Python 3.11 defaults to Boto3 v1.27
-## Remove once AWS Lambda defaults to > v1.28 
+## Remove once AWS Lambda defaults to > v1.28
 import sys
 from pip._internal import main
-main(['install', '-I', '-q', 'boto3', '--target', '/tmp/', '--no-cache-dir', '--disable-pip-version-check'])
-sys.path.insert(0,'/tmp/')
+
+main(
+    [
+        "install",
+        "-I",
+        "-q",
+        "boto3",
+        "--target",
+        "/tmp/",
+        "--no-cache-dir",
+        "--disable-pip-version-check",
+    ]
+)
+sys.path.insert(0, "/tmp/")
 
 
 ## Begin main Lambda function
@@ -31,110 +44,128 @@ import logging
 import os
 
 
-
 def lambda_handler(event, context):
-    
-    LOGGER = logging.getLogger()
-    LOGGER.setLevel(logging.INFO)
-
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
 
     # Retrieve starting parameters from event data or environment variable
     # If parameter not found, inject default values defined in Lambda function
-    if 'WorkspacesDirectory' in event :
-        WorkspacesDirectory = event['WorkspacesDirectory']
-    elif "WorkspacesDirectory" in os.environ :
-        WorkspacesDirectory = os.environ['WorkspacesDirectory']
-    else :
-        WorkspacesDirectory = "UPDATE_DIRECTORYID"
-    
+    if "WorkspacesDirectory" in event:
+        workspaces_directory = event["WorkspacesDirectory"]
+    elif "WorkspacesDirectory" in os.environ:
+        workspaces_directory = os.environ["WorkspacesDirectory"]
+    else:
+        workspaces_directory = "UPDATE_DIRECTORYID"
+
     ## Microsoft Office Professional Plus 2016 - ApplicationId: wsa-khw7gclz4
     ## Microsoft Office Professional Plus 2019 - ApplicationId: wsa-hvh179sq6
-    if 'OfficeAppId' in event :
-        OfficeAppId = event['OfficeAppId']
-    elif "OfficeAppId" in os.environ :
-        OfficeAppId = os.environ['OfficeAppId']
-    else :
-        OfficeAppId = "UPDATE_APPID"
+    if "OfficeAppId" in event:
+        office_app_id = event["OfficeAppId"]
+    elif "OfficeAppId" in os.environ:
+        office_app_id = os.environ["OfficeAppId"]
+    else:
+        office_app_id = "UPDATE_APPID"
 
-    
     workspaces_client = boto3.client("workspaces")
-    
+
     ## Generate the list of application packages to get their Ids
     ## Uncomment below to view the current list of Ids
-    #response = workspaces_client.describe_applications(
+    # response = workspaces_client.describe_applications(
     #    Owner = 'AMAZON'
-    #)
-    #for app in response['Applications']:
+    # )
+    # for app in response['Applications']:
     #    print("Application: " +  app['Name'] + "    ApplicationId: " + app['ApplicationId'])
 
-    print(f"Searching directory {WorkspacesDirectory} for WorkSpaces with app id {OfficeAppId} installed.")
+    print(
+        f"Searching directory {workspaces_directory} for WorkSpaces with app id {office_app_id} installed."
+    )
 
     ## Generate list of WorkSpaces in directory, 25 at a time
     paginator = workspaces_client.get_paginator("describe_workspaces")
-    operation_parameters = {'DirectoryId': WorkspacesDirectory}
+    operation_parameters = {"DirectoryId": workspaces_directory}
     try:
-        ListResponse = {}
-        for page in paginator.paginate(**operation_parameters, PaginationConfig={"PageSize": 25}):
-            if ListResponse:
-                ListResponse["Workspaces"] = (
-                    ListResponse["Workspaces"] + page["Workspaces"]
+        list_response = {}
+        for page in paginator.paginate(
+            **operation_parameters, PaginationConfig={"PageSize": 25}
+        ):
+            if list_response:
+                list_response["Workspaces"] = (
+                    list_response["Workspaces"] + page["Workspaces"]
                 )
             else:
-                ListResponse = {**ListResponse, **page}
-        LOGGER.info("Found %s workspaces in directory %s", len(ListResponse["Workspaces"]), WorkspacesDirectory)
+                list_response = {**list_response, **page}
+        logger.info(
+            "Found %s workspaces in directory %s",
+            len(list_response["Workspaces"]),
+            workspaces_directory,
+        )
 
     except EndpointConnectionError as e:
-        LOGGER.warning("Could not connect to WorkSpaces API endpoint.")
+        logger.warning("Could not connect to WorkSpaces API endpoint.")
     except Exception as e:
-        LOGGER.error("Failed to get WorkSpaces list for directory "+WorkspacesDirectory+" - "+str(e))
+        logger.error(
+            "Failed to get WorkSpaces list for directory "
+            + workspaces_directory
+            + " - "
+            + str(e)
+        )
 
-    if len(ListResponse['Workspaces']) == 0:
-        LOGGER.info("No WorkSpaces instances found in directory "+WorkspacesDirectory)
+    if len(list_response["Workspaces"]) == 0:
+        logger.info("No WorkSpaces instances found in directory " + workspaces_directory)
         return {
-            'statusCode': 200,
-            'body': json.dumps('No WorkSpaces found in directory!')
+            "statusCode": 200,
+            "body": json.dumps("No WorkSpaces found in directory!"),
         }
 
     action_count = 0
-    ## Loop through all WorkSpaces in the directory        
-    for workspace in ListResponse['Workspaces']:
-
+    ## Loop through all WorkSpaces in the directory
+    for workspace in list_response["Workspaces"]:
         ## Get application associations for WorkSpace
         response = workspaces_client.describe_workspace_associations(
-            WorkspaceId = workspace['WorkspaceId'],
+            WorkspaceId=workspace["WorkspaceId"],
             AssociatedResourceTypes=[
-                'APPLICATION',
-            ]
+                "APPLICATION",
+            ],
         )
-        
+
         ## Only check WorkSpaces with associations
-        if response['Associations']:
+        if response["Associations"]:
             ## Check each association looking for Office Application Id
-            for association in response['Associations']:
-                if association['AssociatedResourceId'] == OfficeAppId:
+            for association in response["Associations"]:
+                if association["AssociatedResourceId"] == office_app_id:
                     action_count = action_count + 1
-                    
-                    try :
-                        LOGGER.info("Found matching Office association for" +  workspace['WorkspaceId'])
-                        print("Found matching Office association for " +workspace['WorkspaceId'])
-                        
+
+                    try:
+                        logger.info(
+                            "Found matching Office association for"
+                            + workspace["WorkspaceId"]
+                        )
+                        print(
+                            "Found matching Office association for "
+                            + workspace["WorkspaceId"]
+                        )
+
                         print("Removing application association")
                         response = workspaces_client.disassociate_workspace_application(
-                            WorkspaceId=workspace['WorkspaceId'],
-                            ApplicationId=OfficeAppId
+                            WorkspaceId=workspace["WorkspaceId"],
+                            ApplicationId=office_app_id,
                         )
-                        
+
                         print("Initiating application uninstallation")
                         response = workspaces_client.deploy_workspace_applications(
-                            WorkspaceId=workspace['WorkspaceId'],
-                            Force=True
+                            WorkspaceId=workspace["WorkspaceId"], Force=True
                         )
                     except Exception as e:
-                        LOGGER.error("Failed to get remove application from "+workspace['WorkspaceId']+" - "+str(e))
-                    
+                        logger.error(
+                            "Failed to get remove application from "
+                            + workspace["WorkspaceId"]
+                            + " - "
+                            + str(e)
+                        )
+
     print(f"Initiated Office uninstall on {action_count} WorkSpaces.")
 
     return {
-        'statusCode': 200,
-        'body': json.dumps('Completed WorkSpaces Application Modifications!')
+        "statusCode": 200,
+        "body": json.dumps("Completed WorkSpaces Application Modifications!"),
     }
